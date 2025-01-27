3ab18132e824f6a85c8d8c0410fe72b329566023
Lraafe <Rafael31Alexandre@gmail.com>
2025-01-27 16:09:37 -0300

    Desafios ha fazer

--- 3386-git-github-projeto_inicial/app.js
+++ 3386-git-github-projeto_inicial/app.js
@@ -15,3 +15,10 @@ if(corno == false){
 
 muie = prompt("Em um numero de 1 a 5 quanto sue muié é redical") > 3 ? alert("Tua mulher tá certa em te trair") : alert("Caralho meu parceiro tua ex não é radical e te traiu ainda, tomoulis");
 
+//Crie um novo repositório local
+//Adicione o repositório remoto criado nos exercícios anteriores ao seu repositório local.
+//Faça uma alteração no repositório local e envie para o remoto.
+//Resolva os conflitos manualmente, escolhendo quais alterações serão mantidas
+//Realize um commit para registrar a resolução do conflito.
+//Verifique quais arquivos foram adicionados
+//Sincronize o repositório local com o repositório remoto no GitHub.
